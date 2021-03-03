d8997983001da4e6b50311a914c1e61c7f742880
mochi-MizLab <h20ms419@hirosaki-u.ac.jp>
2021-03-03 13:44:41 +0900

    update: add scrape to reply

--- run.py
+++ run.py
@@ -12,7 +12,7 @@ line_bot_api = LineBotApi(os.environ["ACCESS_TOKEN"])
 handler = WebhookHandler(os.environ["CHANNEL_SECRET"])
 
 api = responder.API()
-# searcher = Scraper()
+searcher = Scraper()
 
 
 @api.route("/")
@@ -48,8 +48,8 @@ async def endpoint(req, resp):
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    line_bot_api.reply_message(event.reply_token,
-                               TextSendMessage(text=event.message.text * 2))
+    rst = searcher.scrape(event.message.text)
+    line_bot_api.reply_message(event.reply_token, TextSendMessage(text=f"{rst}"))
 
 
 if __name__ == "__main__":
